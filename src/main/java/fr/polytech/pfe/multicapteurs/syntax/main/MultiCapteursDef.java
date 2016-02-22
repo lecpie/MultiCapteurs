d2dbe1a29ce7f242bfacbc3f6bddfdc5fba3ec99
Louis Pennachio <pennachiolouis@gmail.com>
2016-02-22 11:08:10 +0100

    changed init model to .java to .groovy but still not working

--- src/main/java/fr/polytech/pfe/multicapteurs/syntax/main/MultiCapteursDef.java
+++ src/main/java/fr/polytech/pfe/multicapteurs/syntax/main/MultiCapteursDef.java
@@ -3,12 +3,18 @@ package fr.polytech.pfe.multicapteurs.syntax.main;
 import fr.polytech.pfe.multicapteurs.syntax.init_dsl.MultiCapteursDefDSL;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Louis on 21/02/2016.
  */
 public class MultiCapteursDef {
-    public static void main(String[] args) {
-        System.out.println("!\n");
+    public static void main(String[] args) throws IOException {
+        MultiCapteursDefDSL dsl = new MultiCapteursDefDSL();
+        if(args.length > 0) {
+            dsl.eval(new File(args[0]));
+        } else {
+            System.out.println("/!\\ Missing arg: Please specify the path to a Groovy script file to execute");
+        }
     }
 }
